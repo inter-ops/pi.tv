baded48408967eb7e981188bee59d968ca163683
Francesco Virga <francescod.virga@gmail.com>
2020-03-27 16:11:06 -0400

    Reorganize files and add imdb search

--- bit-finder/index.js
+++ bit-finder/index.js
@@ -1,121 +1,53 @@
 "use strict";
-const rarbgApi = require('rarbg-api')
+const { searchTorrents, searchImdb } = require("./finder");
+const parser = require("./parser");
 
 const minSeeders = 5
 const maxSize = 5000000000 // 5 GB
 
 const main = async () => {
   try {
-    // NOTE: specific episode codes dont seem to work
-    const torrents = await rarbgApi.search("tt2707408", {
-      limit: 10,
-      sort: "seeders"
-    }, "imdb");
+    // TODO: if not found, search rarbg directly with name
+    const imdbInfo = await searchImdb({ name: "martian", first: true })
+    //console.log(imdbInfo);
 
-    //console.log("length: " + torrents.length);
+    if (!imdbInfo) console.log("Warning: Movie not found in IMDB");
 
-    // TODO: ensure movie name is in torrent title
-    const filtered = torrents.map(t => {
-      t = parseGeneral(t)
-      switch (t.category) {
-        case "TV HD Episodes":
-        case "TV Episodes":
-          return parseTv(t)
-        default:
-          return parseMovie(t);
-      }
-    })
+    const torrents = await searchTorrents({ imdbId: imdbInfo.imdbid })
+    //console.log(torrents);
 
-    console.log("length: " + filtered.length);
-    //console.log(filtered);
+    // TODO: ensure movie name is in parsed torrent title
+    // put this all in the main parse function
+    const filtered = torrents.reduce((filtered, t) => {
+      t = parser.general(t)
 
+      if (t.category.toLowerCase().includes("tv")) t = parser.tv(t);
+      else if (t.category.toLowerCase().includes("movie")) t = parser.movie(t);
+      else t = undefined
+
+      if (t) filtered.push(t)
+
+      return filtered;
+    }, []);
+
+    // TODO: if none, search rarbg directly with name
     if (filtered.length === 0) {
       console.error("No torrents found");
       process.exit(1)
     }
 
-    const chosen = torrents[0]
-    if (chosen.seeders < minSeeders) console.warn("Seeders < 5");
-    if (chosen.size > maxSize) console.warn("Size > 5GB");
+    console.log(`Torrents: ${torrents.length}, after filtering: ${filtered.length}`);
 
-    console.log(chosen);
+    const chosen = filtered[0]
+    console.log("Chosen torrent:\n", chosen);
+
+    if (chosen.seeders < minSeeders) console.warn("Warning: Seeders < 5");
+    if (chosen.size > maxSize) console.warn("Warning: Size > 5GB");
   }
   catch(err) {
     console.error(err);
   }
 }
 
-function parseTv(torrent) {
-  let type;
-  let { title: episode, epnum, seasonnum: ssnum } = torrent.episode_info;
-
-  const match = torrent.title.match(/s(\d{1,2})e?(\d{0,3})/i)
-
-  if (!epnum && match[1]) epnum = match[1]
-
-  if (!epnum || parseInt(epnum) > 5000) {
-    epnum = undefined;
-    type = "Season"
-  }
-  else {
-    type = "Episode"
-    epnum = parseInt(epnum);
-  }
-
-  ssnum = parseInt(ssnum);
-
-  // TODO: format title
-
-  // NOTE: if its a whole season, title looks like this: Season Pack 3
-
-  return {
-    ...torrent,
-    episode,
-    epnum,
-    ssnum,
-    type
-  }
-}
-
-function parseMovie(torrent) {
-  // category = Movies|TV Episodes
-  // codec = x264, XVID
-  // quality = 1080
-
-  const { quality, codec, category } = torrent
-
-  // TODO: format title
-
-  return {
-    ...torrent,
-    type: "Movie"
-  };
-}
-
-function parseGeneral(torrent) {
-
-  const blueray = torrent.title.match(/blu[-_]?ray/i) !== undefined;
-
-  let [category, codec, quality] = torrent.category && torrent.category.split("/")
-
-  if (!quality) {
-    // when quality isnt provided it can sometimes be found in the title
-    const match = torrent.title.match(/\d{3,4}p/i)
-    quality = match && match[0]
-  }
-
-  //console.log(torrent.title);
-
-  // quality can be undefined
-  //return !quality || quality >= 720
-
-  return {
-    ...torrent,
-    quality,
-    blueray,
-    codec,
-    category
-  };
-}
 
 main()
