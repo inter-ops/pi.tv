1be69ce725a6e4e4079f4a399f304452965c8ab5
Francesco Virga <francescod.virga@gmail.com>
2020-03-26 20:50:50 -0400

    Add format-media.sh

---
+++ Transmission/format-media.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO: add any processing required post-download of torrents from tranmission
+
+# env vars
+# TR_APP_VERSION
+# TR_TIME_LOCALTIME
+# TR_TORRENT_DIR
+# TR_TORRENT_HASH
+# TR_TORRENT_ID
+# TR_TORRENT_NAME
